e65d34cc7a0e502c43d55fa38e1d899d6043967c
santiagoaragon <santiagoas77@gmail.com>
2019-10-02 09:08:51 -0500

    Proyecto ya cien por ciento funcional

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyCc8kijv9YI-9G71nF2jr1J9lCnwMwDTyc",
+    authDomain: "vueapp-4f213.firebaseapp.com",
+    databaseURL: "https://vueapp-4f213.firebaseio.com",
+    projectId: "vueapp-4f213",
+    storageBucket: "vueapp-4f213.appspot.com",
+    messagingSenderId: "68355181758",
+    appId: "1:68355181758:web:6435ee2fe3554bdfb662ef"
+}
